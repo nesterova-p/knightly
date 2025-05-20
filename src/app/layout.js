1a2ad29858699980357114e467c1775c9ed9ac4f
nott-po <nesterovapolina55@gmail.com>
2025-05-20 20:34:53 +0200

    - analytics

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import {ClerkProvider} from "@clerk/nextjs";
 import {GlobalContextProvider} from "../app/contextApi";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
     variable: '--font-geist-sans',
@@ -27,6 +28,7 @@ export default function RootLayout({ children }) {
                 className={`${geistSans.variable} ${geistMono.variable} antialiased`}
                 >
                     {children}
+                    <Analytics/>
                 </body>
             </GlobalContextProvider>
         </ClerkProvider>
